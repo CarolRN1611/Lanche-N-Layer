c7636dbc3a84eb6bd8049c2a51b4359cbc3892f0
Caroline Rocha <146491576+CarolRN1611@users.noreply.github.com>
2024-09-02 19:58:49 -0300

    Update IProdutoRepository.java

--- LancheNLayer (4)/LancheNLayer/src/main/java/com/lanchenlayer/interfaces/IProdutoRepository.java
+++ LancheNLayer (4)/LancheNLayer/src/main/java/com/lanchenlayer/interfaces/IProdutoRepository.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 public interface IProdutoRepository {
     public void adicionar(Produto produto);
+    private Produto filtrarProduto(int id);
     public void atualizar(Produto produto,int index);
     public void remover(int id);
     public Produto buscarPorId(int id);
